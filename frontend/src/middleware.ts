5d219974e432d6478152a3aa6969a18bebe229d7
Vaibhav Aayal <vaibhav.aayal@gmail.com>
2026-06-18 10:06:40 +0400

    fix: add __clerk proxy path to middleware matcher

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -27,5 +27,5 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/__clerk/(.*)'],
 };
